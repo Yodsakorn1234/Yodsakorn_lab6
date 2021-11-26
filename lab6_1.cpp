a6b11df2bdea137e977b989762be3a280bf9a01f
Yodsakorn <menming1234@outlook.com>
2021-11-26 14:37:44 +0700

    lab6_1.cpp

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x=1,y=0,i=0;
+     while(x!=0){
+          cout << "Enter an integer: ";
+          cin >> x;
+          if(x==0){
+          break;
+          }
+          if(x%2==0){
+          y++;
+          }
+else{
+    i++;
+}
+}
+    cout << "#Even numbers = "<<y<<"\n";
+    cout << "#Odd numbers = "<<i;
     return 0;
 }
